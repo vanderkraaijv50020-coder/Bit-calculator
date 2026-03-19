f97d9226a7b897df682a69042fd700869d86e861
vanderkraaijv50020-coder <vanderkraaijv50020@masseyhigh.school.nz>
2026-03-20 10:43:17 +1300

    Working Statement Generator

---
+++ C_01_Statement_Generator.py
@@ -0,0 +1,7 @@
+# functions go here
+def statement_generator(statement, decoration):
+    print(f"{decoration * 5} {statement} {decoration * 5}")
+
+
+# main routine goes here
+statement_generator("Instructions", "-")
